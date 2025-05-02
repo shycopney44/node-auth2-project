3b1ecc92b611b062b37064e2afb432368d6dc55c
Shy Copney <shydashacopney@gmail.com>
2025-05-02 15:09:36 -0400

    completed project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require('bcryptjs')
+const Users = require('../users/users-model')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -8,12 +10,20 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
     response:
     status 201
-    {
+    {3
       "user"_id: 3,
       "username": "anna",
       "role_name": "angel"
     }
    */
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, 8)
+  Users.add({ username, password: hash, role_name })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 
@@ -37,6 +47,27 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+    const token = buildToken(req.user)
+    res.json({
+      message: `${req.user.username} is back!`,
+      token,
+    })
+  } else {
+    next({ status: 401, message: 'Invalid credentials' })
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name,
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,19 @@ function find() {
       }
     ]
    */
+
+// select
+//     user_id,
+//     username,
+//     role_name
+// from users
+// join roles on
+//     users.role_id = roles.role_id;
+
+return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+
 }
 
 function findBy(filter) {
@@ -33,7 +46,21 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+ select
+    user_id,
+    username,
+    password,
+    role_name
+from users
+join roles on
+    users.role_id = roles.role_id
+where users.user_id = 1;
    */
+
+return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +74,12 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where('users.user_id', user_id).first()
+
 }
 
 /**
